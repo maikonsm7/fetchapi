e374cea16d7b16cfc77be285ca0bee629da3e402
Maikon Monteiro <maikonsm7@gmail.com>
2024-06-08 21:28:32 -0400

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 const url = 'https://jsonplaceholder.typicode.com/posts'
+
 const loading = document.querySelector("#loading")
 const postsContainer = document.querySelector("#posts-container")
 
+const postPage = document.querySelector("#post")
+const postContainer = document.querySelector("#post-container")
+const commentsContainer = document.querySelector("#comments-container")
+
+// Get id from url
+const urlSearchParams = new URLSearchParams(window.location.search)
+const postId = urlSearchParams.get('id')
+
 // Get all posts
-async function getAllPosts(){
+async function getAllPosts() {
     const res = await fetch(url)
     const data = await res.json()
     loading.classList.add('hide')
@@ -26,4 +35,50 @@ async function getAllPosts(){
     })
 }
 
-getAllPosts()
+async function getPost(id) {
+    try {
+        // para mais de um fetch assíncrono, é melhor usar o Promise.all
+        const [responsePost, responseComments] = await Promise.all([
+            fetch(`${url}/${id}`), // buscar o post
+            fetch(`${url}/${id}/comments`) // buscar os comentários do post acima
+        ])
+
+        const dataPost = await responsePost.json()
+        const dataComments = await responseComments.json()
+
+        loading.classList.add('hide')
+        postPage.classList.remove('hide')
+
+        const title = document.createElement('h1')
+        const body = document.createElement('p')
+
+        title.innerText = dataPost.title
+        body.innerText = dataPost.body
+
+        postContainer.appendChild(title)
+        postContainer.appendChild(body)
+
+        dataComments.map(comment => {
+            const div = document.createElement("div")
+            const email = document.createElement("h3")
+            const commentBody = document.createElement("p")
+
+            email.innerText = comment.email
+            commentBody.innerText = comment.body
+
+            div.appendChild(email)
+            div.appendChild(commentBody)
+
+            commentsContainer.appendChild(div)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+if (!postId) {
+    getAllPosts()
+} else {
+    getPost(postId)
+}
